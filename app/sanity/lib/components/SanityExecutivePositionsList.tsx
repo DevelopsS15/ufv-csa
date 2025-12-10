41e9f1f1c419f881585be1c88cbb5c2ef9c340a8
DevelopsS15 <42367017+DevelopsS15@users.noreply.github.com>
2025-12-09 22:11:16 -0800

    fix: mismatch hook rendering when displaying a prettier executive position list

--- app/sanity/lib/components/SanityExecutivePositionsList.tsx
+++ app/sanity/lib/components/SanityExecutivePositionsList.tsx
@@ -6,79 +6,57 @@ import { Skeleton } from "~/app/(site)/components/UI/skeleton";
 import { longMonthDayYearDateFormatOption } from "~/app/(site)/utils";
 
 export function SanityExecutivePositionsList(props) {
-  const validProps = {
-    ...props,
-    renderPreview: (itemProp) => CustomPreviewComponent(itemProp, props),
-  };
-  return validProps.renderDefault(validProps);
-}
+  const [positions, setPositions] = React.useState<{ _id: string; title: string }[]>([]);
+  const [retrievingPositions, setRetrievingPositions] = React.useState<boolean>(true);
 
-function CustomPreviewComponent(itemProp, props) {
-  const [positionName, setPositionName] = React.useState<string | null>(null);
-  const [fetchingPositionName, setFetchingPositionName] =
-    React.useState<boolean>(false);
-
-  const itemValue = itemProp.value;
   React.useEffect(() => {
-    if (itemValue._type !== "termDuration") return;
-    const itemPositionRef = itemValue.position?._ref;
-    if (!itemValue.position || !itemPositionRef) return;
+    const hasAnyPositions = props.value.filter((item) => item.position && item.position._ref).length > 0;
+    if (!hasAnyPositions) return;
+
     const runAsync = async () => {
       try {
-        setFetchingPositionName(true);
-        const data = await readClientWithCDN.fetch(
-          groq`*[_type=="executivePositions" && _id == $ref][0]`,
-          {
-            ref: itemPositionRef,
-          }
-        );
-        if (typeof data?.title === `string`) {
-          setPositionName(data.title);
-        }
-      } catch (e) {
-        console.error(e);
+        setRetrievingPositions(true);
+        const data = await readClientWithCDN.fetch(groq`*[_type=="executivePositions"]{_id, title}`);
+        setPositions(data);
+      } catch (error) {
+        console.error(error);
       } finally {
-        setFetchingPositionName(false);
+        setRetrievingPositions(false);
       }
-    };
+    }
     runAsync();
-  }, [itemValue]);
+  }, []);
 
-  if (itemValue._type === "termDuration") {
-    const startDateInstance = new Date(itemValue.startDate);
-    const endDateInstance = new Date(itemValue.endDate);
-    const isValidStart = !isNaN(startDateInstance.getTime());
-    const isValidEnd = !isNaN(endDateInstance.getTime());
-    return (
-      <>
-        {fetchingPositionName ? (
-          <Skeleton className="h-4 w-full" />
-        ) : (
-          <div>{positionName ?? "Unknown position"}</div>
-        )}
-        <div>
-          {isValidStart || isValidEnd ? (
-            <>
-              {isValidStart
-                ? startDateInstance.toLocaleDateString(
-                    undefined,
-                    longMonthDayYearDateFormatOption
-                  )
-                : "Unknown"}{" "}
-              to{" "}
-              {isValidEnd
-                ? endDateInstance.toLocaleDateString(
-                    undefined,
-                    longMonthDayYearDateFormatOption
-                  )
-                : "unknown"}
-            </>
-          ) : (
-            "Unknown duration"
-          )}
-        </div>
-      </>
-    );
-  }
-  return props.renderPreview(itemProp);
+  return props.renderDefault({
+    ...props,
+    renderPreview: (itemProp) => {
+      // This is the main custom rendering for each item in the Positions list. 
+      // This allows us to display the position title and formatted term duration, instead of raw references and date strings.
+      if (itemProp.value._type === "termDuration") {
+        const startDateInstance = new Date(itemProp.value.startDate);
+        const endDateInstance = new Date(itemProp.value.endDate);
+        const isValidStart = !isNaN(startDateInstance.getTime());
+        const isValidEnd = !isNaN(endDateInstance.getTime());
+
+        const positionRef = itemProp.value.position?._ref;
+        const positionData = positions.find((pos) => pos._id === positionRef);
+        const positionName = positionData ? positionData.title : null;
+
+        let durationString = "Unknown duration";
+        if (isValidStart || isValidEnd) {
+          const startString = isValidStart ? startDateInstance.toLocaleDateString(undefined, longMonthDayYearDateFormatOption) : "Unknown";
+          const endString = isValidEnd ? endDateInstance.toLocaleDateString(undefined, longMonthDayYearDateFormatOption) : "unknown";
+          durationString = `${startString} to ${endString}`;
+        }
+
+        return (
+          <>
+            {retrievingPositions ? (<Skeleton className="h-4 w-full" />) : (<div>{positionName ?? "Unknown position"}</div>)}
+            <div>{durationString}</div>
+          </>
+        );
+      }
+      return props.renderPreview(itemProp);
+    }
+  });
 }
